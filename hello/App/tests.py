1c560bd8f628d959117ceaffdd555b08bae49fda
mrthere3 <871780968@qq.com>
2021-04-23 17:21:20 +0800

    second test

--- hello/App/tests.py
+++ hello/App/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.urls import resolve
 from App.views import home_page
+from django.http import HttpRequest
 
 # Create your tests here.
 class HomePageTest(TestCase):
@@ -8,3 +9,11 @@ class HomePageTest(TestCase):
         # self.assertEqual(1+1,3)
         found=resolve('/')
         self.assertEqual(found.func,home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response=home_page(request)
+        html = response.content.decode('utf-8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>',html)
+        self.assertTrue(html.endswith('</html>'))
